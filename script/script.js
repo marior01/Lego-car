275a028ebbf127c7767dcc221732dee484f87818
marior01 <mary14042020@gmail.com>
2024-10-12 15:55:08 +0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function(){
+  $('.carousele').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: true, // Відображення крапок для навігації
+    arrows: true, // Відображення стрілок
+  });
+});
+
+var pathname = window.location.pathname;
+if (!pathname.includes('404')) {
+  window.location.href = './404.html';
+}
